b3c8379bbce1ddebff1107c6f6171851456b2655
Akshat Singh <132469517+Akshat041@users.noreply.github.com>
2025-02-22 17:12:35 +0530

    Changed question on modal window

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Gather-Up
-Welcome to GatherUp, your go-to platform for staying updated on all college events! We aim to streamline event management by providing a centralized hub where students, faculty, and organizers can access event details effortlessly.
+
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const Description = document.querySelector("#description");
 const Locat = document.querySelector("#location");
 const Interested = document.querySelector("#isInterested");
 
-const addBook = document.querySelector(".add-book");
+const addEvent = document.querySelector(".add-event");
 const modal = document.querySelector("#modal");
 const dialogForm = document.querySelector("#bookForm");
 
@@ -23,9 +23,9 @@ function Book(title, descriptio, location, isread) {
 }
 
 // creates original object and store that object in myLibrary array.
-function addBookToLibrary(title, descriptio, location, read) {
+function addBookToLibrary(title, descriptio, location, Interested = false) {
   // original object
-  const book = new Book(title, descriptio, location, read);
+  const book = new Book(title, descriptio, location, Interested);
   myLibrary.push(book);
 }
 
@@ -55,7 +55,9 @@ function displayBook(library) {
 
     const readBtn = document.createElement("button");
     readBtn.classList.add("read");
-    readBtn.textContent = `${book.isread == "true" ? "Interested" : "Not Interested"}`;
+    readBtn.textContent = `${
+      book.isread == "true" ? "Interested" : "Not Interested"
+    }`;
 
     readBtn.addEventListener("click", () => {
       if (readBtn.textContent == "Interested") {
@@ -78,27 +80,39 @@ function displayBook(library) {
 
     title.textContent = book.title;
     descriptio.textContent = book.descriptio;
-    location.textContent = `${book.location} location`;
+    location.textContent = `Venue: ${book.location}`;
 
     bottomBtns.appendChild(readBtn);
     bottomBtns.appendChild(deleteBtn);
-    cardBottom.appendChild(location);
     cardBottom.appendChild(bottomBtns);
     card.appendChild(title);
     card.appendChild(descriptio);
     card.appendChild(cardBottom);
+    card.appendChild(location);
     booksContainer.appendChild(card);
   });
 }
 
-addBookToLibrary("Atomic Habits", "James Clear", 306, false);
 addBookToLibrary(
-  "The 7 Habits of Highly Effective People",
-  "Stephen R. Covey",
-  250,
+  "FOSS Hack United",
+  "You all are informed that premier free and open source software (FOSS) Hackathon is happening on 22 to 23 Feb, 2025",
+  "CSJMU (Kanpur)",
   false
 );
-addBookToLibrary("Rich Dad, Poor Dad", "Robert T. Kiyosaki", 345, false);
+
+addBookToLibrary(
+  "TechKriti '25",
+  "Dive into the future with Techkriti '25, Asia's largest technical and entrepreneurial festival, from 27th to 30th March, 2025",
+  "IIT Kanpur",
+  false
+);
+
+addBookToLibrary(
+  "ENGENIOUS - (Cultural Tech Fest)",
+  "Experience a vibrant fusion of tradition and innovation. Expect digital art, tech-enhanced performances. Join us on 24 to 29 March, 2025",
+  "KIT (On-Campus)",
+  false
+);
 
 function handleNewBookFormSubmit(event) {
   event.preventDefault();
@@ -107,6 +121,8 @@ function handleNewBookFormSubmit(event) {
   const descriptio = document.querySelector("#descriptio").value;
   const location = document.querySelector("#location").value;
   const read = document.querySelector("#read").value;
+  console.log(read);
+  //   const read = false;
 
   addBookToLibrary(title, descriptio, location, read);
   displayBook(myLibrary);
@@ -121,13 +137,18 @@ function clearInputs() {
   Interested.value = false;
 }
 
-addBook.addEventListener("click", () => {
+addEvent.addEventListener("click", () => {
   // event.preventDefault();
   modal.showModal();
 });
 
 confirm.addEventListener("click", () => {
-  addBookToLibrary(Title.value, Description.value, Locat.value, Interested.value);
+  addBookToLibrary(
+    Title.value,
+    Description.value,
+    Locat.value,
+    Interested.value = false
+  );
   displayBook(myLibrary);
   clearInputs();
 });
